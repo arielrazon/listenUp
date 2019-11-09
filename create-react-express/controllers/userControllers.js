083c128b99771af836c4448af84641157f5cc1b7
Jahaan Rasheed <jahaanrasheed@Jahaans-MacBook-Pro.local>
2019-11-08 20:54:27 -0500

    made functions for creating and getting account from database

---
+++ create-react-express/controllers/userControllers.js
@@ -0,0 +1,32 @@
+const db = require('../models');
+
+module.exports = {
+    create: (req, res) => {
+        db.User.create({
+          username: req.body.username,
+          password: req.body.password,
+          email: req.body.email
+        }).then((err,data)=>{
+            if (err) {
+                res.sendStatus(500)
+            } else {
+                res.sendStatus(200)
+            }
+        })
+    },
+    getAccount: (req,res) => {
+        db.User.find({
+           username: req.body.username,
+           password: req.body.password,
+        }).then((err,data)=>{
+             if (err) {
+                 res.sendStatus(500)
+             } else if (!data) {
+                 res.sendStatus(404)
+             } else {
+                 res.sendStatus(200)
+                 res.json(200)
+             }
+        })
+    }
+}
