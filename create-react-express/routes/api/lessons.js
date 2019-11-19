df89e5aec26d11533105dd9771fb8e717b8c852e
Jahaan Rasheed <jahaanrasheed@Jahaans-MacBook-Pro.local>
2019-11-19 17:43:43 -0500

    not using controllers anymore in stead calling db from the routes

--- create-react-express/routes/api/lessons.js
+++ create-react-express/routes/api/lessons.js
@@ -1,31 +1,94 @@
 const router = require("express").Router();
-const lessonController = require("../../controllers/lessonControllers");
-const progressController = require("../../controllers/progressControllers");
-const pointsController = require("../../controllers/pointsControllers");
-const userController = require("../../controllers/userControllers");
+const db = require("../../models");
 
+router.post("/create", (req, res) => {
+    db.User.create({
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email
+    }).then(data => {
+        res.sendStatus(200)
+    }, error => {
+        console.log(error);
+        res.sendStatus(500)
+    }).catch(err => res.status(422).json(err))
+})
+router.get("/login",(req,res)=>{
+    db.User.find({
+        username: req.body.username,
+        password: req.body.password,
+    }).populate("Lesson", "Progress", "Points")
+        .then( data => {
+            
+             if (!data) {
+                res.sendStatus(404)
+            } else {
+                res.json(data)
+            }
+        },error =>{
+            console.log(error)
+            res.sendStatus(500)
+        }).catch(err => res.status(422).json(err))
+})
+router.put("/addPoints",(req,res)=>{
+    const _id = req.body._id
+    const points = req.body.points
+    db.Points.updateOne({
 
-router.route("/create")
-.post(userController.create)
-router.route("/login")
-.get(userController.getAccount)
-router.route("/lessons")
-.get(lessonController.findAll);
-router.route("/play/:name")
-.get(lessonController.findModule);
-router.route("/addpoints")
-.put(pointsController.addPoints);
-router.route("/mod1Two")
-.put(progressController.updateMod1Two);
-router.route("/mod1Three")
-.put(progressController.updateMod1Two);
-router.route("/mod2Two")
-.put(progressController.updateMod2Two);
-router.route("/mod2Three")
-.put(progressController.updateMod2Three);
-router.route("/mod3Two")
-.put(progressController.updateMod3Two);
-router.route("/mod3Three").put(progressController.updateMod3Three);
+    }, { $set: { Points: Points + points } }).then(data => {
+    
+        res.sendStatus(200)
+        return db.User.findOneandUpdateOne({
+            _id: ObjectId(_id)
+        }, { $push: { Points: data._id } })
+        
+    }, error => {
+        console.log(error)
+        res.sendStatus(500)
+    }).catch(err => res.status(422).json(err)) 
+})
+router.get("/findAll",(req,res)=>{
+    db.Lesson.find({}).then(data => {
+        if (!data) {
+            res.sendStatus(404)
+        } else {
+            res.json(data);
+        }
+    }, error => {
+        console.log(error);
+        res.sendStatus(500)
+    }).catch(err => res.status(422).json(err))
+})
+router.get("/findModule",(req,res)=>{
+    db.Lesson.find({
+        moduleName: req.body.name,
+    }).then(data => {
+        if (!data) {
+            res.sendStatus(404)
+        } else {
+            res.json(data)
+        }
+    }, error => {
+        console.log(error);
+        res.sendStatus(500)
+    }).catch(err => res.status(422).json(err))
+})
+router.get("findCategory",(req,res)=>{
+    db.Lesson.find({
+        moduleName: req.body.name,
+        category: req.body.category,
+    }).then(data => {
+        if (!data) {
+            res.sendStatus(404)
+        } else {
+            res.json(data)
+        }
+    }, error => {
+        console.log(error);
+        res.sendStatus(500)
+    }).catch(err => res.status(422).json(err))
+})
+
 
 
 module.exports = router
