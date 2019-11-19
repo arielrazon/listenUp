6efee898f7a1439f7e42eb879a6c94d98816ef72
Jahaan Rasheed <jahaanrasheed@Jahaans-MacBook-Pro.local>
2019-11-19 18:41:20 -0500

    changes to database schemas, added more routes, add info to package.json

--- create-react-express/routes/api/lessons.js
+++ create-react-express/routes/api/lessons.js
@@ -13,41 +13,41 @@ router.post("/create", (req, res) => {
         res.sendStatus(500)
     }).catch(err => res.status(422).json(err))
 })
-router.get("/login",(req,res)=>{
+router.get("/login", (req, res) => {
     db.User.find({
         username: req.body.username,
         password: req.body.password,
     }).populate("Lesson", "Progress", "Points")
-        .then( data => {
-            
-             if (!data) {
+        .then(data => {
+
+            if (!data) {
                 res.sendStatus(404)
             } else {
                 res.json(data)
             }
-        },error =>{
+        }, error => {
             console.log(error)
             res.sendStatus(500)
         }).catch(err => res.status(422).json(err))
 })
-router.put("/addPoints",(req,res)=>{
+router.put("/addPoints", (req, res) => {
     const _id = req.body._id
     const points = req.body.points
     db.Points.updateOne({
 
     }, { $set: { Points: Points + points } }).then(data => {
-    
+
         res.sendStatus(200)
         return db.User.findOneandUpdateOne({
             _id: ObjectId(_id)
         }, { $push: { Points: data._id } })
-        
+
     }, error => {
         console.log(error)
         res.sendStatus(500)
-    }).catch(err => res.status(422).json(err)) 
+    }).catch(err => res.status(422).json(err))
 })
-router.get("/findAll",(req,res)=>{
+router.get("/findAll", (req, res) => {
     db.Lesson.find({}).then(data => {
         if (!data) {
             res.sendStatus(404)
@@ -59,7 +59,7 @@ router.get("/findAll",(req,res)=>{
         res.sendStatus(500)
     }).catch(err => res.status(422).json(err))
 })
-router.get("/findModule",(req,res)=>{
+router.get("/findModule", (req, res) => {
     db.Lesson.find({
         moduleName: req.body.name,
     }).then(data => {
@@ -73,7 +73,7 @@ router.get("/findModule",(req,res)=>{
         res.sendStatus(500)
     }).catch(err => res.status(422).json(err))
 })
-router.get("findCategory",(req,res)=>{
+router.get("/findCategory", (req, res) => {
     db.Lesson.find({
         moduleName: req.body.name,
         category: req.body.category,
@@ -88,8 +88,18 @@ router.get("findCategory",(req,res)=>{
         res.sendStatus(500)
     }).catch(err => res.status(422).json(err))
 })
+router.put("/updateMod1", (req,res)=>{
+    db.Progress.updateOne({
+            
+    },{$set:{Module1: req.body.percentage}}).then(data=>{
+          
+              res.sendStatus(200)
+              return db.User.findOneandUpdateOne({_id:ObjectId(req.body._id)},{$push:{Progress: data._id}},{new: true})
 
-
+    },error =>{
+        console.log(error)
+        res.sendStatus(500)
+    }).catch(err => res.status(422).json(err))
+})
 
 module.exports = router
-
