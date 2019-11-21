44ad0bd3ef4a31a9d799b89d7a51e86e851d8ddb
Peter Cole <Peter_Cole1@yahoo.com>
2019-11-20 20:54:16 -0500

    changed logo and fixed welcome div

--- create-react-express/client/src/components/Welcome/index.js
+++ create-react-express/client/src/components/Welcome/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.css";
-import logo from "../../img/logo1.png";
+import logo from "../../img/logo2.png";
 
 function Welcome() {
   return (
@@ -15,13 +15,7 @@ function Welcome() {
         </div>
         <div className="col-7 greeting2">
           <h3>ListenUp</h3>
-          <h4 className="welcomeBack">
-            Welcome Back Dear Precious User, whom I would die for and relinquish
-            all of my worldy goods to protect from the bad influences of the
-            world, including but not limited to: muggers, knives, guns,
-            assassins, badgers, honey badgers, lions, olives, Jake Paul, and low
-            quality memes. Nice to see you again.
-          </h4>
+          <h4 className="welcomeBack">Welcome Back. Nice to see you again.</h4>
         </div>
       </div>
     </div>
